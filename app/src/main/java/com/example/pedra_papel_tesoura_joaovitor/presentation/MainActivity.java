0fab346eddfcbe46c9c995ac327023c29371b2f9
João Vitor <joao.vitor@maxdata.com.br>
2024-04-24 16:56:39 -0300

    first commit

---
+++ app/src/main/java/com/example/pedra_papel_tesoura_joaovitor/presentation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.pedra_papel_tesoura_joaovitor.presentation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pedra_papel_tesoura_joaovitor.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setupToolbar();
+    }
+
+
+    private void setupToolbar() {
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+    }
+}
